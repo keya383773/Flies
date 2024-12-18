29502c645393ec0548fa407187b70701a75b2a21
keya383773 <kifayat4444@outlook.com>
2024-12-18 12:39:56 +0600

    Update Downloads.bat

--- Downloads.bat
+++ Downloads.bat
@@ -1,6 +1,4 @@
 @echo off
-curl -L -o loop.bat https://www.dropbox.com/scl/fi/vji7ekyslpbovokpqeay3/loop.bat?rlkey=876nfzm3qdmyqhc1jckgqjcld&dl=0
-curl -L -o show.bat https://www.dropbox.com/scl/fi/cwbwdo2n3tt8rbqmugc6h/show.bat?rlkey=41m0ds12mg6e28giib3zqlf6w&dl=0
 pip install pyautogui --quiet
 pip install psutil --quiet
 curl -s -L -o time.py https://www.dropbox.com/scl/fi/ox42qglbf6fsnm9erf8cw/timelimit.py?rlkey=opyeqgum1k95kud81xlc7d66r&dl=0
